b51321226c47b0561d7b389df7b067f420d09e13
PC_TIP209_21 <znffna@gmail.com>
2023-09-18 15:04:27 +0900

    Drill 2 제출 (2020184022 윤영민)

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,42 @@
 from pico2d import *
+import math
 
-open_canvas()
+open_canvas(800,800)
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+t = 0
+course = 0
+x = 400
+y = 90
+while(True):          
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+
+    if course == 0: # 원        
+        x = 300 * math.cos((t-90) / 360 * 2 * math.pi) + 400
+        y = 300 * math.sin((t-90) / 360 * 2 * math.pi) + 400
+        
+    if course == 1: # 사각형
+        if (t + 45)//90 == 0 :
+            x = x + 8;
+        if (t + 45)//90 == 1 :
+            y = y + 7;
+        if (t + 45)//90 == 2 :
+            x = x - 8;
+        if (t + 45)//90 == 3 :
+            y = y - 7;
+        if (t + 45)//90 == 4 :
+            x = x + 8;
+            
+    t = t + 1
+    if t == 360:
+        course = course + 1
+        course = course % 2
+    t = t % 360            
+    
+    delay(0.01)
 
 close_canvas()
