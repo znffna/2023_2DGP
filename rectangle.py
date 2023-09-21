1c5e41b54ea215a4593bea18a8f39f6dde09de02
PC_TIP209_25 <znffna@gmail.com>
2023-09-21 10:35:52 +0900

    그림 출력 함수화 완료
    render_frame(x,y)

--- rectangle.py
+++ rectangle.py
@@ -6,6 +6,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)  
+
 def run_circle():
     print('CIRCLE')
     
@@ -16,28 +22,24 @@ def run_circle():
     for deg in range(0,360,1):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.01)  
+        render_frame(x,y)
 
 def run_rectangle():
     print('RECTANGLE')
 
-    for x in range(50,750+1,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)  
-
+    for x in range(50, 750 + 1, 10):
+        render_frame(x,90)    
+        
+#    for x in range(750, 50 - 1, -10):
+#        render_frame(x,90)
 
     
     pass
 
 
 while True:
-    run_rectangle()
-    # run_circle()
+    run_circle()
+    run_rectangle()   
     break
 
 close_canvas()
