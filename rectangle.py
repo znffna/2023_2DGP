bb2048b3b4b2928b4efd5e5e0e3dc03ff4475cb1
PC_TIP209_25 <znffna@gmail.com>
2023-09-21 10:23:29 +0900

    사각형 맨 밑줄 그림

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import math
+
 open_canvas()
 
 grass = load_image('grass.png')
@@ -18,18 +19,25 @@ def run_circle():
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        delay(0.01)
-
-    pass
+        delay(0.01)  
 
 def run_rectangle():
     print('RECTANGLE')
+
+    for x in range(50,750+1,10):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.01)  
+
+
+    
     pass
 
 
 while True:
-    run_circle()
     run_rectangle()
+    # run_circle()
     break
 
 close_canvas()
