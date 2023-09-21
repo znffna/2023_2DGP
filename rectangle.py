bb37141a7aefa46b106de0030373fb1420be6c88
PC_TIP209_25 <znffna@gmail.com>
2023-09-21 10:03:23 +0900

    화면에 잔디와 캐릭터 그리기 성공

--- rectangle.py
+++ rectangle.py
@@ -7,6 +7,13 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
+    
+    # 일단 그림을 그리자
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
+    
     pass
 
 def run_rectangle():
@@ -17,5 +24,6 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
 
 close_canvas()
