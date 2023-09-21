a9fae66e338bc6f9fca67dffa296f58ea7709b14
PC_TIP209_25 <znffna@gmail.com>
2023-09-21 10:40:52 +0900

    사각형 운동 완성

--- rectangle.py
+++ rectangle.py
@@ -36,7 +36,9 @@ def run_rectangle():
     for x in range(750, 50 - 1, -10):
         render_frame(x,550)
 
-    
+    for y in range(550, 90 - 1, -10):
+        render_frame(50 ,y)
+        
     pass
 
 
