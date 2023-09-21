032aa1b100149a958898a0d935b5d12f3ad9ce89
PC_TIP209_25 <znffna@gmail.com>
2023-09-21 10:38:09 +0900

    사각형 윗줄 작성완료

--- rectangle.py
+++ rectangle.py
@@ -30,15 +30,15 @@ def run_rectangle():
     for x in range(50, 750 + 1, 10):
         render_frame(x,90)    
         
-#    for x in range(750, 50 - 1, -10):
-#        render_frame(x,90)
+    for x in range(750, 50 - 1, -10):
+        render_frame(x,550)
 
     
     pass
 
 
 while True:
-    run_circle()
+    # run_circle()
     run_rectangle()   
     break
 
