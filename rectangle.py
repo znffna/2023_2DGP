fb5cc083866f1bf6f2e3f2334519e7a441925a8c
PC_TIP209_25 <znffna@gmail.com>
2023-09-21 09:58:56 +0900

    무한루프 뼈대 완성
    원과 사각형을 무한루프하는 코드 작성

--- rectangle.py
+++ rectangle.py
@@ -5,6 +5,17 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    print('CIRCLE')
+    pass
 
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
+while True:
+    run_circle()
+    run_rectangle()
 
 close_canvas()
