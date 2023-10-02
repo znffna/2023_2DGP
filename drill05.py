c9ee201282d0b8bb2b06057e17f2290a74feb3f1
znffna <znffna@gmail.com>
2023-10-03 08:46:09 +0900

    x, y 를 cx, cy 로 변수 이름 변경

--- drill05.py
+++ drill05.py
@@ -25,10 +25,10 @@ def handle_events():
 
 
 def reset_world():
-    global running, x, y, frame
+    global running, cx, cy, frame
     global hx, hy
     running = True
-    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
 
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
@@ -39,7 +39,7 @@ def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     arrow.draw(hx, hy)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
     update_canvas()
 
 
