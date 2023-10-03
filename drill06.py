02634da8ca06d31eb049a03889d29ca56519eb5b
znffna <znffna@gmail.com>
2023-10-03 11:11:14 +0900

    reformat

--- drill06.py
+++ drill06.py
@@ -42,7 +42,7 @@ def reset_world():
     frame = 0
     action = 3
 
-    points = [ ]
+    points = []
     set_new_target_arrow()
 
 
@@ -61,7 +61,7 @@ def set_new_target_arrow():
         frame = 0
         target_exists = True
     else:
-        action = 3 if action == 1 else 2    # 이전에 소년이 우측으로 이동중이었으면, IDLE 동작시 우측을 바라보도록
+        action = 3 if action == 1 else 2  # 이전에 소년이 우측으로 이동중이었으면, IDLE 동작시 우측을 바라보도록
         frame = 0
         target_exists = False
 
@@ -88,9 +88,9 @@ def update_world():
             cx = (1 - t) * sx + t * hx  # cx 는 시작 x 와 끝 x 를 1-t:t 의 비율로 섞은 위치
             cy = (1 - t) * sy + t * hy
             t += 0.001
-        else:   # 목표지점에 도달하면
+        else:  # 목표지점에 도달하면
             cx, cy = hx, hy  # 캐릭터 위치를 기존 목적지 위치와  강제로 정확이 일치시킴.
-            del points[0]   # 목표지점에왔기 때문에, 더 이상 필요없는 점을 삭제
+            del points[0]  # 목표지점에왔기 때문에, 더 이상 필요없는 점을 삭제
             set_new_target_arrow()
     elif points:  # 목표 지점이 없는 상황에서, 새로운 목표 지점이 생기면...
         set_new_target_arrow()
