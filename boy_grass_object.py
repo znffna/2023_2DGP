929250eb1d80c90d98d7247e4aa28aa917270969
znffna <143989887+znffna@users.noreply.github.com>
2023-10-05 10:42:15 +0900

    소년 클래스 작성 및 객체 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -11,6 +11,19 @@ class Grass:
     def update(self): pass
 
 
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
 def handle_events():
     global running
     events = get_events()
@@ -25,18 +38,22 @@ def handle_events():
 def reset_world():
     global running
     global grass
+    global boy
 
     running = True
     grass = Grass()
+    boy = Boy()
 
 def update_world():
     grass.update()
+    boy.update()
     pass
 
 
 def render_world():
     clear_canvas()
     grass.draw()
+    boy.draw()
     update_canvas()
     pass
 
