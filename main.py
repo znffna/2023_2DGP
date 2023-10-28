311aba86556d0bac8a390480b99673f9eeea247c
znffna <znffna@gmail.com>
2023-10-28 12:27:53 +0900

    main : 기본 게임 종료 이벤트 작성

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def create_world():
 
 
 def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
     pass
 
 
