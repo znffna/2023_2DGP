9f982779d57119f9f4c6ae7956ab01cb19d3155b
znffna <znffna@gmail.com>
2023-10-28 17:31:38 +0900

    main : 선수 클래스 생성

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from pico2d import *
 
 
+class Player:
+    image = None
+
+    def __init__(self):
+        self.x, self.y = 400, 60
+        if Player.image == None:
+            Player.image = load_image('resource/character.png')
+        pass
+
+    def update(self):
+        pass
+
+    def handle_event(self):
+        pass
+
+    def draw(self):
+        pass
+
+    pass
+
+
 def create_world():
     global running
     running = True
+
+    player = Player()
+
     pass
 
 
