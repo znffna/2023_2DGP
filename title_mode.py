e8ddaa1a7a14cb903db6e066c95220b2956ae636
znffna <znffna@gmail.com>
2023-12-04 10:21:16 +0900

    space바 입력으로 게임 시작

--- title_mode.py
+++ title_mode.py
@@ -1,4 +1,4 @@
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
 
 import game_framework
 from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time, load_music
@@ -27,7 +27,7 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN:
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
             game_framework.change_mode(play_mode)
 
 
